6a3fbb55b52e9eaba22e5e363c49f9bf3436f52e
Tyralion <piliaiev@gmail.com>
2012-09-11 13:34:40 +0600

    Исправлена ошибка с определение корневой директории Rails-приложения.

--- lib/imp/trap.rb
+++ lib/imp/trap.rb
@@ -73,7 +73,11 @@ module Imp
 end # Imp
 
 if defined?(::Rails)
-  ::Imp::Trap::FILE_LOCK = "#{::Rails.root}/tmp/imp_#{::Imp::Trap.mpid}.lock".freeze
+
+  dir = File.join(::Rails.root || ::Dir.pwd, "tmp")
+  ::FileUtils.mkdir_p(dir, :mode => 0755) unless ::FileTest.directory?(dir)
+  ::Imp::Trap::FILE_LOCK = ::File.join(dir, "imp_#{::Imp::Trap.mpid}.lock").freeze
+
 else
   ::Imp::Trap::FILE_LOCK = "/tmp/imp_#{::Imp::Trap.mpid}.lock".freeze
 end
